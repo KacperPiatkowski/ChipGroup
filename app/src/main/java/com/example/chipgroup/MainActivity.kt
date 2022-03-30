77f8ee9bc9452f57e29261f216b8df7bcd1c301e
KacperPiatkowski <k.piatkowski07@gmail.com>
2022-03-30 08:30:37 +0200

    skonczony

---
+++ app/src/main/java/com/example/chipgroup/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.chipgroup
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.chipgroup.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
